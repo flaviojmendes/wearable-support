2513a390422d21b5760c46fe229f210b388c00bc
Flavio Mendes <flaviojmendes@Flavios-MacBook-Pro.local>
2018-08-20 22:37:57 +0100

    retrieving weather from Openweather API

--- app/services/openweather.service.js
+++ app/services/openweather.service.js
@@ -9,10 +9,39 @@
 //     }
 // });
 
+const request = require('request')
+const Q = require('q')
 
-var retrieveWeather = function() {
-    return '{"var": "test"}'
+const url = "http://api.openweathermap.org/data/2.5/weather?units=imperial&lat={lat}&lon={lon}&appid={appid}"
+
+function retrieveWeather(lat, lon, callback) {
+    let repURL = url.replace('{lat}', lat).replace('{lon}', lon).replace('{appid}', '1455382c9be6c3db4fe8f894230202b7')
+    request.get(repURL, (err, res, body) => {
+       if(err) {
+           return console.dir(err)
+       }
+
+       let weather = {};
+       let weatherJSON = JSON.parse(body);
+
+       weather.lat = lat;
+       weather.lon = lon;
+       weather.con = weatherJSON.weather[0].main;
+       weather.loc = weatherJSON.name;
+       weather.srise = weatherJSON.sys.sunrise;
+       weather.sset = weatherJSON.sys.sunset;
+       weather.tmpC = (((weatherJSON.main.temp - 32) * 5) / 9).toFixed(1);
+       weather.tmpF = weatherJSON.main.temp.toFixed(1);
+       weather.hum = weatherJSON.humidity;
+
+
+       callback(null, weather);
+    });
 }
+
+
+
+
 
 module.exports = {
     retrieveWeather: retrieveWeather
--- routes/weather.controller.js
+++ routes/weather.controller.js
@@ -2,13 +2,19 @@ const express = require('express');
 const router = express.Router();
 const openWeatherService = require('../app/services/openweather.service')
 
-router.get('/:lat/:long', function(req, res, next) {
+router.get('/:lat/:lon', function(req, res, next) {
 
-    if(req.params.lat && req.params.long) {
+    if (req.params.lat && req.params.lon) {
         res.setHeader('Content-Type', 'application/json')
-        res.send(openWeatherService.retrieveWeather())
+
+        openWeatherService.retrieveWeather(req.params.lat, req.params.lon, function (err, body) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.send(body);
+            }
+        });
     }
 });
-
 
 module.exports = router;
